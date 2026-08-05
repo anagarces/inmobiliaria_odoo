d3da50143677492ea7001702c6c0fff735a028a4
Ana Garces <anagarcestech@gmail.com>
2026-08-05 10:22:07 +0200

    se crea modelo para los tipos de propiedades

---
+++ addons/estate/models/estate_property_type.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class Estate_Property_Type(models.Model):
+
+    _name = "estate.property.type"
+    _description = "Property Type"
+
+    name = fields.Char(required=True)
