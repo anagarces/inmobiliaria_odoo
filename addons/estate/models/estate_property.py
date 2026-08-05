612a18d83369b0b1290f0dfd35d69cae7add265e
Ana Garces <anagarcestech@gmail.com>
2026-08-05 10:41:30 +0200

    se ananden los campos relacionados en estate.property model y las vistas/action del estate.property.type model

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -42,3 +42,6 @@ class EstateProperty(models.Model):
         default="new",
         copy=False
     )
+    property_type_id = fields.Many2one("estate.property.type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer")
+    seller_id = fields.Many2one("res.partner", string="Seller")
--- addons/estate/models/estate_property_type.py
+++ addons/estate/models/estate_property_type.py
@@ -1,6 +1,6 @@
 from odoo import models, fields
 
-class Estate_Property_Type(models.Model):
+class EstatePropertyType(models.Model):
 
     _name = "estate.property.type"
     _description = "Property Type"
