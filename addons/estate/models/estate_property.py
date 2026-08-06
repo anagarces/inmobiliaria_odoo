172e79e306d5ca8ea4dc0dc29f4e55dd5efbf487
Ana Garces <anagarcestech@gmail.com>
2026-08-06 12:41:13 +0200

    se anade campo calculado  best_price para obtener la mejor oferta de una propiedad y se actualiza la vista form de estate-property

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -48,8 +48,17 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
     total_area = fields.Float(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price")
 
+
+    #Calcular area total de una propiedad
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    #Obtener la mejor oferta de una propiedad
+    @api.depends("offer_ids")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'), default=0)
