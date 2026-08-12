c37c194c54991431c587c7e481e667c5407430c4
Ana Garces <anagarcestech@gmail.com>
2026-08-12 13:25:04 +0200

    se anade nuevos campos en modelo offer y type, y un stat button para los tipos de propiedades y mostrar ofertas por tipo de propiedad

--- addons/estate/models/estate_property_offer.py
+++ addons/estate/models/estate_property_offer.py
@@ -21,6 +21,12 @@ class EstatePropertyOffer(models.Model):
     property_id = fields.Many2one('estate.property', required=True)
     validity = fields.Integer(default=7)
     date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    property_type_id = fields.Many2one(
+    "estate.property.type",
+    related="property_id.property_type_id",
+    string="Property Type",
+    store=True
+)
 
     #SQL constraint
 
--- addons/estate/models/estate_property_type.py
+++ addons/estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class EstatePropertyType(models.Model):
 
@@ -9,6 +9,13 @@ class EstatePropertyType(models.Model):
     name = fields.Char(required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id')
     sequence = fields.Integer('Sequence', default=10)
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offers")
+    offer_count = fields.Integer(compute="_compute_offer_count", string="Offer Count")
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
 
     #SQL constraint
     _sql_constraints = [
